46d52adfd73a17d68aa45b9bf34fbf53a06c71d3
Aaro Salosensaari <aaro.salosensaari@cs.helsinki.fi>
2016-05-19 17:29:00 +0300

    use >> instead of >

--- code/list_all_art_advs.py
+++ code/list_all_art_advs.py
@@ -12,9 +12,9 @@ if __name__ == "__main__":
 
     # pipe all filenames to file
     for year in xrange(1870, 1910):
-        pr = os.popen("find /srv/data/newspapers/newspapers/fin/" + str(year) +" /* -name '*article*.txt' > random_sample_gigantic_art.txt")
+        pr = os.popen("find /srv/data/newspapers/newspapers/fin/" + str(year) +" /* -name '*article*.txt' >> random_sample_gigantic_art.txt")
         pr.close()
-        pr = os.popen("find /srv/data/newspapers/newspapers/fin/" + str(year) +" /* -name '*adv*.txt' > random_sample_gigantic_adv.txt")
+        pr = os.popen("find /srv/data/newspapers/newspapers/fin/" + str(year) +" /* -name '*adv*.txt' >> random_sample_gigantic_adv.txt")
         pr.close()
 
 
