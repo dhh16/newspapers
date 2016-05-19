554baf6a7d3d54b31f6fabbb07b864befa2c80c4
Aaro Salosensaari <aaro.salosensaari@helsinki.fi>
2016-05-19 07:47:56 +0000

    debug

--- code/dhh_preprocess_tools.py
+++ code/dhh_preprocess_tools.py
@@ -60,12 +60,14 @@ def hfst_words(wordlist):
     found_words = set()
     for l in pr:
         lsplit = l.split()
+        if not lsplit:
+            continue
         w_o = lsplit[0]
         if w_o not in found_words:
             rest = lsplit[1]
             if rest:
                 rsplit = rest.split(']')
-                if rsplit[0] and rsplit[0][1:].startwith("WORD_ID"):
+                if rsplit[0] and rsplit[0][1:].startswith("WORD_ID"):
                     out.append(rsplit[0][7:])
     pr.close()
     return out
