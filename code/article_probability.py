9cc3afe32c5e39282ce70bc4c895aee334f35baa
Ville Vaara <villepvaara@gmail.com>
2016-05-20 09:45:40 +0300

    dumdidum

--- code/article_probability.py
+++ code/article_probability.py
@@ -63,7 +63,6 @@ def read_files_from_textfile_to_list(file_list_textfile):
     return file_list
 
 
-
 def string_from_list(file_list):
     results_string = ""
     print file_list
@@ -72,24 +71,22 @@ def string_from_list(file_list):
     return results_string
 
 
-
-
 ###########################
-#checking for the probability
+# checking for the probability
 ############################
 
 def article_prob():
     out_f5 = open("Final", 'w')
     input_article_list = read_files_from_textfile_to_list("randomish_sample_3000_articles.txt")
-    #i =0
+    # i = 0
     for in_list in input_article_list:
         input_string_article = ""
-        input_string_article =  string_from_list(in_list) # read file to string
+        input_string_article = string_from_list(in_list)  # read file to string
         article_token = token_func(input_string_article)
         out_f = open("output_file_article", 'w')
         out_f1 = open("output_file_article_final", 'w')
         for item in article_token:
-            out_f.write(item.encode('utf8')+ "\n")
+            out_f.write(item.encode('utf8') + "\n")
         out_f.close()
         in_string = read_file_to_string("machinereadable_output_file_500_stopwords_filtered.txt")
         in_token = token_func(in_string)
@@ -97,7 +94,7 @@ def article_prob():
         lab = in_token[1].split(",")
         sumi = 0.0
         for item in article_token:
-            for i in range(0,len(in_token)-1):
+            for i in range(0, len(in_token) - 1):
                 l = in_token[i].split(",")[0]
                 if item == l:
                     out_f_l = unicode(item + "\t" + in_token[i].split(",")[1])
@@ -105,176 +102,75 @@ def article_prob():
                     sumi = sumi + float(in_token[i].split(",")[1])
 
         print "The final sum(values)/amount of is \n"
-        if le >0:
-            lll = sumi/le
+        if le > 0:
+            lll = sumi / le
         else:
             lll = 0
-        out_ff = unicode(in_list+"\t"+ "," +  str(lll))
-        out_f5.write(out_ff.encode("utf") +"\n")
+        out_ff = unicode(in_list + "\t" + "," + str(lll))
+        out_f5.write(out_ff.encode("utf") + "\n")
     write_Final_readable()
 
 def write_Final_readable():
-    out_final_readable = open("Final_hyperlink.txt","w")
+    out_final_readable = open("Final_hyperlink.txt", "w")
     out_final_machine = open("Final_machinereadable.txt", "w")
-    f = open("Final","r")
-    answer ={}
-    sorted_list2 ={}
+    f = open("Final", "r")
+    answer = {}
+    sorted_list2 = {}
     for line in f:
-        k,v = line.strip().split(",")
+        k, v = line.strip().split(",")
         answer[k.strip()] = v.strip()
     f.close()
     sorted_list = sorted(answer.items(), key=lambda i: float(i[1]), reverse=True)
-    for j in range(0,len(sorted_list)):
+    for j in range(0, len(sorted_list)):
         start = sorted_list[j][0].find("extracted")
         end_sup = sorted_list[j][0].find("supplement")
         end_sec = sorted_list[j][0].find("section")
-        if end_sup>0:
-            xmml_path = sorted_list[j][0][:start]+"alto"+sorted_list[j][0][start+9:end_sup]+"001.xml"
-        elif end_sec>0:
-            xmml_path = sorted_list[j][0][:start]+"alto"+sorted_list[j][0][start+9:end_sec]+"001.xml"
+        if end_sup > 0:
+            xmml_path = (sorted_list[j][0][:start] +
+                         "alto" + sorted_list[j][0][start + 9:end_sup] +
+                         "001.xml")
+        elif end_sec > 0:
+            xmml_path = (sorted_list[j][0][:start] +
+                         "alto" + sorted_list[j][0][start + 9:end_sec] +
+                         "001.xml")
         else:
             end = sorted_list[j][0].find("article")
-            xmml_path = sorted_list[j][0][:start]+"alto"+sorted_list[j][0][start+9:end]+"001.xml"
+            xmml_path = (sorted_list[j][0][:start] +
+                         "alto" + sorted_list[j][0][start + 9:end] +
+                         "001.xml")
         e = ET.parse(xmml_path).getroot()
-        out_xff = unicode("Serial Number = " + str(j) +"\n" +
-                            e[0][16].text + "\n" + str(sorted_list[j][1])
-                            + "\n" + sorted_list[j][0] + "\n"+
-                            "********************************" + "\n")
-        out_final_readable.write(out_xff.encode("utf") +"\n")
+        out_xff = unicode("Serial Number = " + str(j) + "\n" +
+                          e[0][16].text + "\n" + str(sorted_list[j][1]) +
+                          "\n" + sorted_list[j][0] + "\n" +
+                          "********************************" + "\n")
+        out_final_readable.write(out_xff.encode("utf") + "\n")
 
         out_machine = sorted_list[j][0] + "," + str(sorted_list[j][1])
-        out_final_machine.write(out_machine.encode("utf") +"\n")
+        out_final_machine.write(out_machine.encode("utf") + "\n")
 
     out_final_readable.close()
     out_final_machine.close()
 
-article_prob()
 
-
-
-
-"""
-def article_prob():
-        input_article_list = read_files_from_textfile_to_list("input_article.txt")
-        input_string_article = ""
-        input_string_article =  string_from_list(input_article_list) # read file to string
-
-        article_token = token_func(input_string_article)
-        out_f = open("output_file_article", 'w')
-        out_f1 = open("output_file_article_final", 'w')
-        for item in article_token:
-                out_f.write(item.encode('utf8')+ "\n")
-        out_f.close()
-        in_string = read_file_to_string("machinereadable_output_file_500_stopwords_filtered.txt")
-        in_token = token_func(in_string)
-        le = len(article_token)
-        lab = in_token[1].split(",")
-        sumi = 0.0
-        for item in article_token:
-                for i in range(0,len(in_token)-1):
-                        l = in_token[i].split(",")[0]
-                        if item == l:
-                                out_f_l = unicode(item + "\t" + in_token[i].split(",")[1])
-                                out_f1.write(out_f_l.encode("utf") + "\n")
-                                sumi = sumi + float(in_token[i].split(",")[1])
-        print "The final sum(values)/amount of is \n"
-        print sumi/le
-article_prob()
-
-
-"""
-"""
-# for the first file
-
-input_file = "test_input.txt"
-output_file = "test_output.txt"
-
-print "\nprocessing immigration testdata\n"
-
-articles = read_files_from_textfile_to_list("test_training_data_list.txt")
-
-input_string1 = string_from_list(articles)
-tokens1 = token_func(input_string1)
-length_of_tokenlist1 = len(tokens1)
-diction1 = token_dict(tokens1)
-final_diction1 = token_percentage(diction1, length_of_tokenlist1)
-print final_diction1
-
-
-# the second file preparation
-
-articles = read_files_from_textfile_to_list("test_training_data_list.txt")
-
-# print type(articles)
-
-input_string3 = string_from_list(articles)
-
-# for item in articles:
-#     print "\nprocessing item: " + item
-#     # file3 = item
-#     input_string3 += read_file_to_string(item)
-
-print "\ntokenizing..."
-tokens3 = token_func(input_string3)
-print "\nadding first tokens to this one..."
-tokens3.extend(tokens1)
-print "\ncounting length: "
-length_of_tokenlist3 = len(tokens3)
-print ("\n" + str(length_of_tokenlist3) + " tokens")
-print "\ncreating dictionary..."
-diction3 = token_dict(tokens3)
-print "\ncalculating percentages..."
-final_diction3 = token_percentage(diction3, length_of_tokenlist3)
-
-###################
-
-# input_file2 = "test_input2.txt"
-# # output_file2 = "test_output2.txt"
-
-# input_string2 = read_file_to_string(input_file2)
-
-# tokens2 = token_func(input_string2)
-# length_of_tokenlist2 = len(tokens2)
-# diction2 = token_dict(tokens2)
-# final_diction2 = token_percentage(diction2)
-# print final_diction2
-
-
-differences_dict = {}
-for key in final_diction1:
-    if key in final_diction3:
-        difference = round(final_diction1[key][1] - final_diction3[key][1], 5)
-        count_in_1 = final_diction1[key][0]
-        count_in_2 = final_diction3[key][0]
-        differences_dict[key] = [difference, count_in_1, count_in_2]
-
-# print differences_dict
-
-# if you want to sort with some other item in the list change the secnod position in the lambda
-sorted_list = sorted(differences_dict.items(), key=lambda i: i[1][0], reverse=True)
-
-# print sorted_list
-
-shortened_list = sorted_list[:100]
-
-
-def write_pretty_output(output_file):
-    out_f = open(output_file, 'w')
-    for item in shortened_list:
-        outputline = unicode(item[0] + "   " +
-                             str(item[1][0] * 100) +
-                             " %  " +
-                             str(item[1][1]) +
-                             " items\n")
-
-        out_f.write(outputline.encode('utf8'))
-    out_f.close()
-
-print "\nwriting top 100 into outputfile\n"
-write_pretty_output("nice_output_file.txt")
-
-# out_f = open(output_file, 'w')
-# for item in shortened_list:
-#     out_f.write(str(item) + "\n")
-
-"""
+def write_final_readable_textfilelinks():
+    out_final_readable = open("../data/cr2_textfiles.txt", "w")
+    out_final_machine = open("Final_machinereadable.txt", "w")
+    f = open("Final", "r")
+    answer = {}
+    # sorted_list2 = {}
+    for line in f:
+        k, v = line.strip().split(",")
+        answer[k.strip()] = v.strip()
+    f.close()
+    sorted_list = sorted(answer.items(), key=lambda i: i[1], reverse=True)
+    shortened_list = sorted_list[:50]
+    for j in range(0, len(shortened_list)):
+        out_xff = unicode("Serial Number = " + str(j) +
+                          "\n" + str(sorted_list[j][1]) + "\n" +
+                          "86.50.168.175" + (sorted_list[j][0])[4:] + "\n" +
+                          "********************************" + "\n")
+        out_final_readable.write(out_xff.encode("utf") + "\n")
+    out_machine = sorted_list[j][0] + "," + str(sorted_list[j][1])
+    out_final_machine.write(out_machine.encode("utf") + "\n")
+    out_final_readable.close()
+    out_final_machine.close()
